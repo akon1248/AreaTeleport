a07d1f658166e30f2d0ce13ac33d3708e3c3f26f
akon1248 <akon8321@gmail.com>
2020-07-30 01:40:56 +0900

    登録名に使える文字の制限

--- src/main/java/com/akon/areateleport/command/CommandRegister.java
+++ src/main/java/com/akon/areateleport/command/CommandRegister.java
@@ -24,42 +24,46 @@ public class CommandRegister extends CommandAbstract {
 			return false;
 		}
 		if (sender instanceof Player) {
-			if (AreaTeleport.getTeleportAreaManager().getTeleportArea(args[0]) == null) {
-				try {
-					Region selection = AreaTeleport.getWEInstance().getSession((Player)sender).getSelection(new BukkitWorld(((Player) sender).getWorld()));
-					BlockVector3 min = selection.getMinimumPoint();
-					BlockVector3 max = selection.getMaximumPoint();
-					BoundingBox boundingBox = new BoundingBox(min.getX(), min.getY(), min.getZ(), max.getX() + 1, max.getY() + 1, max.getZ() + 1);
-					if (!boundingBox.overlaps(((Player)sender).getBoundingBox())) {
-						Bukkit.getScheduler().runTaskAsynchronously(AreaTeleport.getInstance(), () -> {
-							TeleportArea overlappedArea = null;
-							for (TeleportArea teleportArea: AreaTeleport.getTeleportAreaManager().getTeleportAreas(((Player)sender).getWorld())) {
-								if (teleportArea.getArea().overlaps(boundingBox)) {
-									overlappedArea = teleportArea;
-									break;
+			if (!args[0].matches("[0-9a-zA-Z_\\-]")) {
+				if (AreaTeleport.getTeleportAreaManager().getTeleportArea(args[0]) == null) {
+					try {
+						Region selection = AreaTeleport.getWEInstance().getSession((Player) sender).getSelection(new BukkitWorld(((Player) sender).getWorld()));
+						BlockVector3 min = selection.getMinimumPoint();
+						BlockVector3 max = selection.getMaximumPoint();
+						BoundingBox boundingBox = new BoundingBox(min.getX(), min.getY(), min.getZ(), max.getX() + 1, max.getY() + 1, max.getZ() + 1);
+						if (!boundingBox.overlaps(((Player) sender).getBoundingBox())) {
+							Bukkit.getScheduler().runTaskAsynchronously(AreaTeleport.getInstance(), () -> {
+								TeleportArea overlappedArea = null;
+								for (TeleportArea teleportArea : AreaTeleport.getTeleportAreaManager().getTeleportAreas(((Player) sender).getWorld())) {
+									if (teleportArea.getArea().overlaps(boundingBox)) {
+										overlappedArea = teleportArea;
+										break;
+									}
 								}
-							}
-							if (overlappedArea == null) {
-								AreaTeleport.getTeleportAreaManager().registerTeleportArea(new TeleportArea(args[0], ((Player)sender).getLocation(), boundingBox));
-								MessageUtil.sendMessage(sender, "&6テレポートエリアを登録しました: {0}", args[0].toLowerCase());
-								try {
-									AreaTeleport.getTeleportAreaManager().updateYaml(args[0]);
-								} catch (IOException ex) {
-									MessageUtil.sendMessage(sender, "&cテレポートエリアの保存に失敗しました");
-									ex.printStackTrace();
+								if (overlappedArea == null) {
+									AreaTeleport.getTeleportAreaManager().registerTeleportArea(new TeleportArea(args[0], ((Player) sender).getLocation(), boundingBox));
+									MessageUtil.sendMessage(sender, "&6テレポートエリアを登録しました: {0}", args[0].toLowerCase());
+									try {
+										AreaTeleport.getTeleportAreaManager().updateYaml(args[0]);
+									} catch (IOException ex) {
+										MessageUtil.sendMessage(sender, "&cテレポートエリアの保存に失敗しました");
+										ex.printStackTrace();
+									}
+								} else {
+									MessageUtil.sendMessage(sender, "&c指定された範囲が他のテレポートエリアと重なっています: {0}", overlappedArea.getName());
 								}
-							} else {
-								MessageUtil.sendMessage(sender, "&c指定された範囲が他のテレポートエリアと重なっています: {0}", overlappedArea.getName());
-							}
-						});
-					} else {
-						MessageUtil.sendMessage(sender, "&c選択範囲の外に出てください");
+							});
+						} else {
+							MessageUtil.sendMessage(sender, "&c選択範囲の外に出てください");
+						}
+					} catch (IncompleteRegionException e) {
+						MessageUtil.sendMessage(sender, "&c範囲を選択してください");
 					}
-				} catch (IncompleteRegionException e) {
-					MessageUtil.sendMessage(sender, "&c範囲を選択してください");
+				} else {
+					MessageUtil.sendMessage(sender, "&c{0}という名前のテレポートエリアは既に登録されています", args[0].toLowerCase());
 				}
 			} else {
-				MessageUtil.sendMessage(sender, "&c{0}という名前のテレポートエリアは既に登録されています", args[0].toLowerCase());
+				MessageUtil.sendMessage(sender, "&c使用できない文字が含まれています");
 			}
 		} else {
 			MessageUtil.sendMessage(sender, "&cこのコマンドはプレイヤーからのみ実行可能です");
